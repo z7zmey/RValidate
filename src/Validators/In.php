2f01870fcd2904cef5a85b51658eaedc5ec21176
z7zmey <z7zmey@gmail.com>
2015-09-21 14:53:24 +0300

    improve error message for validator "In"

--- src/Validators/In.php
+++ src/Validators/In.php
@@ -9,17 +9,26 @@ class In implements Interfaces\Validator
 {
     private $haystack;
     private $strict;
+    private $message;
     
     public function __construct(array $haystack, $strict = false)
     {
         $this->haystack = $haystack;
         $this->strict = $strict;
+
+
+        $haystackStr = [];
+        foreach ($haystack as $item) {
+            $haystackStr[] = is_scalar($item)? "'{$item}'": '|' . gettype($item) . '|';
+        }
+        
+        $this->message = 'mast be in [' . implode(', ', $haystackStr) . ']';
     }
     
     public function validate($data) 
     {
         if (!in_array($data, $this->haystack, $this->strict)) {
-            throw new Exceptions\ValidateException('mast be in array');
+            throw new Exceptions\ValidateException($this->message);
         }
         
         return true;
